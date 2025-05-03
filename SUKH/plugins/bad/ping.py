4a93d12a25a91f522e47b9f684185123b17fec7a
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-05-03 16:26:34 +0530

    Update ping.py

--- SUKH/plugins/bad/ping.py
+++ SUKH/plugins/bad/ping.py
@@ -1,1 +1,12 @@
+from pyrogram import filters
+from pyrogram.types import Message
+import time
+from SUKH import ap
 
+@app.on_message(filters.command(["ping", "alive"]))
+async def ping(_, message: Message):
+    start_time = time.time()
+    sent_message = await message.reply_text("Pinging...")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    await sent_message.edit_text(f"Pong! 🏓\nResponse time: {ping_time} ms")
