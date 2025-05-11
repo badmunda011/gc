d102bcf382edc4bcf041e51f6f428f25e5c6746a
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-11 19:10:29 +0530

    Update start.py

--- SUKH/plugins/bad/start.py
+++ SUKH/plugins/bad/start.py
@@ -1,15 +1,12 @@
-import logging
 import os
 import platform
 import psutil
 import time
-import json
 
 from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
-from config import BOT_USERNAME, OWNER_ID
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import BOT_USERNAME
 from SUKH import app
-from config import *
 
 # Constants
 START_TEXT = """<b>🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️</b>
@@ -20,30 +17,8 @@ START_TEXT = """<b>🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️
 ᴊᴜsᴛ ᴀᴅᴅ ᴛʜɪs ʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ɪᴛ ᴀɴ ᴀᴅᴍɪɴ\n
 ғᴇᴇʟ ғʀᴇᴇ ғʀᴏᴍ ᴀɴʏ ᴛʏᴘᴇ ᴏғ **ᴄᴏᴘʏʀɪɢʜᴛ** 🛡️
 """
-AUTHORIZED_USERS_FILE = "authorized_users.json"
+
 MAX_MESSAGE_LENGTH = 20
-Devs = ["7588172591"]
-
-# Define gd_buttons
-gd_buttons = [
-    [InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/JARVIS_V2"),
-     InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="back_to_start"),
-     InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/JARVIS_V_SUPPORT")]
-]
-
-# Load authorized users from file
-def load_authorized_users():
-    if os.path.exists(AUTHORIZED_USERS_FILE):
-        with open(AUTHORIZED_USERS_FILE, "r") as f:
-            return json.load(f)
-    return [OWNER_ID]
-
-# Save authorized users to file
-def save_authorized_users(users):
-    with open(AUTHORIZED_USERS_FILE, "w") as f:
-        json.dump(users, f)
-
-AUTHORIZED_USERS = load_authorized_users()
 
 # Bot Functionality
 start_time = time.time()
@@ -67,7 +42,6 @@ def size_formatter(bytes: int) -> str:
 async def start_command_handler(_, msg):
     buttons = [
         [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-        [InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="vip_back")]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
     await msg.reply_photo(
@@ -93,71 +67,8 @@ async def activevc(_, message: Message):
     )
     await message.reply(reply_text, quote=True)
 
-@app.on_message(filters.command("auth") & filters.user(OWNER_ID))
-async def auth_user(_, message: Message):
-    if len(message.command) != 2:
-        await message.reply_text("Usage: /auth <user_id>")
-        return
-
-    user_id = int(message.command[1])
-    if user_id not in AUTHORIZED_USERS:
-        AUTHORIZED_USERS.append(user_id)
-        save_authorized_users(AUTHORIZED_USERS)
-        await message.reply_text(f"User {user_id} has been authorized.")
-    else:
-        await message.reply_text(f"User {user_id} is already authorized.")
-
-@app.on_message(filters.command("unauth") & filters.user(OWNER_ID))
-async def unauth_user(_, message: Message):
-    if len(message.command) != 2:
-        await message.reply_text("Usage: /unauth <user_id>")
-        return
-
-    user_id = int(message.command[1])
-    if user_id in AUTHORIZED_USERS:
-        AUTHORIZED_USERS.remove(user_id)
-        save_authorized_users(AUTHORIZED_USERS)
-        await message.reply_text(f"User {user_id} has been unauthorized.")
-    else:
-        await message.reply_text(f"User {user_id} is not authorized.")
-
-@app.on_message(filters.command("listauth") & filters.user(OWNER_ID))
-async def list_authorized_users(_, message: Message):
-    if not AUTHORIZED_USERS:
-        await message.reply_text("No users are currently authorized.")
-        return
-
-    authorized_users = "\n".join(map(str, AUTHORIZED_USERS))
-    await message.reply_text(f"Authorized users:\n{authorized_users}")
-
-# Callback Query Handlers
-@app.on_callback_query(filters.regex("vip_back"))
-async def vip_back_callback_handler(_, query: CallbackQuery):
-    await query.message.edit_caption(caption=START_TEXT, reply_markup=InlineKeyboardMarkup(gd_buttons))
-
-@app.on_callback_query(filters.regex("back_to_start"))
-async def back_to_start_callback_handler(_, query: CallbackQuery):
-    await query.answer()
-    await query.message.delete()
-    await start_command_handler(_, query.message)
-
 # Message Handlers
-async def delete_long_edited_messages(client, edited_message: Message):
-    if edited_message.from_user.id in AUTHORIZED_USERS or edited_message.from_user.id in Devs:
-        return
-    if edited_message.text and len(edited_message.text.split()) > 20:
-        await edited_message.reply_text(f"{edited_message.from_user.mention}, ʏᴏᴜʀ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
-        await edited_message.delete()
-    elif edited_message.sticker or edited_message.animation or edited_message.emoji:
-        return
-
-@app.on_edited_message(filters.group & ~filters.me)
-async def handle_edited_messages(_, edited_message: Message):
-    await delete_long_edited_messages(_, edited_message)
-
 async def delete_long_messages(client, message: Message):
-    if message.from_user.id in AUTHORIZED_USERS or message.from_user.id in Devs:
-        return
     if message.text and len(message.text.split()) > MAX_MESSAGE_LENGTH:
         await message.reply_text(f"{message.from_user.mention}, ᴘʟᴇᴀsᴇ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴍᴇssᴀɢᴇ sʜᴏʀᴛ.")
         await message.delete()
