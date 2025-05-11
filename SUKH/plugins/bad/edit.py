a71bca16864a124940ceb32c37ae2cc12ecbf467
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-11 16:40:56 +0530

    Update edit.py

--- SUKH/plugins/bad/edit.py
+++ SUKH/plugins/bad/edit.py
@@ -1,11 +1,13 @@
 import os
 import json
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from collections import defaultdict
 from telegram import Update, Message
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from telegram.constants import ParseMode
 from config import OWNER_ID
-from SUKH import application
+from SUKH import application, app
 
 AUTHORIZED_USERS_FILE = "authorized_users.json"
 
@@ -98,6 +100,32 @@ async def handle_edited_message(update: Update, context: CallbackContext):
         except Exception as e:
             print(f"Failed to delete message: {e}")
 
+
+
+# Delete long messages
+async def delete_long_messages(client, message: Message):
+    if message.from_user and message.from_user.id in AUTHORIZED_USERS + list(map(int, Devs)):
+        return
+    if message.text and len(message.text.split()) > MAX_MESSAGE_LENGTH:
+        await message.reply_text(f"{message.from_user.mention}, ᴘʟᴇᴀsᴇ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴍᴇssᴀɢᴇ sʜᴏʀᴛ.")
+        await message.delete()
+
+# Delete long edited messages
+async def delete_long_edited_messages(client, edited_message: Message):
+    if edited_message.from_user and edited_message.from_user.id in AUTHORIZED_USERS + list(map(int, Devs)):
+        return
+    if edited_message.text and len(edited_message.text.split()) > 20:
+        await edited_message.reply_text(f"{edited_message.from_user.mention}, ʏᴏᴜʀ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ.")
+        await edited_message.delete()
+
+# Handlers
+@app.on_message(filters.group & ~filters.me)
+async def handle_messages(_, message: Message):
+    await delete_long_messages(_, message)
+
+@app.on_edited_message(filters.group & ~filters.me)
+async def handle_edited_messages(_, edited_message: Message):
+    await delete_long_edited_messages(_, edited_message)
 
 
 # Bot Handlers
