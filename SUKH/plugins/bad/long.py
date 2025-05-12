c9654ee999c45797b062fce2de67c85bc372d1a8
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-12 13:07:29 +0530

    Update long.py

--- SUKH/plugins/bad/long.py
+++ SUKH/plugins/bad/long.py
@@ -6,7 +6,7 @@ from SUKH import application
 # Function to handle long messages
 async def handle_long_message(update: Update, context: CallbackContext):
     message: Message = update.message
-    if message and len(message.text.split()) > 50:  # Check if the message has more than 50 words
+    if message and len(message.text.split()) > 60:  # Check if the message has more than 50 words
         try:
             chat_id = message.chat_id
             message_id = message.message_id
@@ -16,11 +16,11 @@ async def handle_long_message(update: Update, context: CallbackContext):
             await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
 
             # Send warning message
-            warning_text = f"⚠️ {user.mention_html()}, your message exceeds 50 words and has been deleted."
+            warning_text = f"⚠️ {user.mention_html()}, ʏᴏᴜʀ ᴍᴇꜱꜱᴀɢᴇ ᴇxᴄᴇᴇᴅꜱ 60 ᴡᴏʀᴅꜱ ᴀɴᴅ ʜᴀꜱ ʙᴇᴇɴ ᴅᴇʟᴇᴛᴇᴅ."
             await context.bot.send_message(chat_id=chat_id, text=warning_text, parse_mode=ParseMode.HTML)
 
         except Exception as e:
-            print(f"Failed to delete message: {e}")
+            print(f"ꜰᴀɪʟᴇᴅ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴍᴇꜱꜱᴀɢᴇ: {e}")
 
 # Create the application instance
 app_instance = application
