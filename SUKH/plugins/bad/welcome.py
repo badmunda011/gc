47c5f5e498172eca94c065c280141a8eb0d79dda
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-12 14:04:54 +0530

    Update welcome.py

--- SUKH/plugins/bad/welcome.py
+++ SUKH/plugins/bad/welcome.py
@@ -5,13 +5,13 @@ from telegram.ext import CommandHandler, MessageHandler, filters
 # Welcome New Users
 async def welcome(update: Update, context):
     for user in update.message.new_chat_members:
-        welcome_message = f"{user.mention_html()}) • 🌸 ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ?"
+        welcome_message = f"{user.mention_html()} • 🌸 **ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ** ?"
         await update.message.reply_text(welcome_message, parse_mode='HTML')
 
 # Goodbye User
 async def goodbye(update: Update, context):
     user = update.message.left_chat_member
-    goodbye_message = f"{user.first_name} (@{user.username if user.username else 'username'}) • 🌪️ **ɢᴏᴏᴅ ʙʏᴇ ᴛᴀᴋᴇ ᴄᴀʀᴇ**."
+    goodbye_message = f"{user.mention_html()} • 🌪️ **ɢᴏᴏᴅ ʙʏᴇ ᴛᴀᴋᴇ ᴄᴀʀᴇ** ."
     await update.message.reply_text(goodbye_message, parse_mode='HTML')
 
 # Bot Handlers
