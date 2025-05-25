d18537d50702cbf02e69ee0e84cee262961ace46
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-25 14:32:25 +0530

    Delete SUKH/plugins/bad/Ab.py

--- SUKH/plugins/bad/Ab.py
+++
@@ -1,6 +0,0 @@
-from SUKH import app
-from pyrogram.modules.edit_text_delete import get_edited_text_delete_handler
-
-def register_edit_handler():
-    handler = get_edited_text_delete_handler()
-    app.add_handler(handler)
